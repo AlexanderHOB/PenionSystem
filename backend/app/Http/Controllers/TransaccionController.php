b3824e4911e6f1c8e40e710f3a5688b1dae7682b
Alexander Henry <obispoa1@gmail.com>
2019-07-02 10:00:34 -0500

    Modulo Transaccion Methodo GET

--- backend/app/Http/Controllers/TransaccionController.php
+++ backend/app/Http/Controllers/TransaccionController.php
@@ -19,7 +19,27 @@ class TransaccionController extends Controller
         $historiales[]=$empleado;
         $historiales[0]['transacciones']=$descuentos;
 
-        $total=Transaccion::selectRaw('sum(monto),persona_id,fecha_inicio')
+        $total=Transaccion::selectRaw('sum(monto) AS total,persona_id,fecha_inicio')
+            ->groupBy('persona_id','fecha_inicio')
+            ->where('persona_id','=',$buscar)
+            ->whereDate('fecha_inicio', $fecha)
+            ->get();
+
+            return [$historiales,$total];    
+    }
+    public function descuento(Request $request)
+    {
+       //Buscar Historial por ID y Fecha
+        $buscar =   $request->id;
+        $fecha  =   $request->fecha;
+
+        $empleado=Empleado::where('empleados.id','=',$buscar)->first();
+        $descuentos=Transaccion::whereDate('fecha_inicio',$fecha)->where('persona_id','=',$buscar)->get();
+        //Agregamos los datos a un arreglo
+        $historiales[]=$empleado;
+        $historiales[0]['transacciones']=$descuentos;
+
+        $total=Transaccion::selectRaw('sum(monto) AS total,persona_id,fecha_inicio')
             ->groupBy('persona_id','fecha_inicio')
             ->where('persona_id','=',$buscar)
             ->whereDate('fecha_inicio', $fecha)
